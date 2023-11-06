65af20be82ffa3448703862099a1aa58fea89547
Hans Naert <hans.naert@vives.be>
2023-11-06 23:05:38 +0100

    GUI_VNC with MQTT echo

---
+++ GUI_VNC.c
@@ -0,0 +1,96 @@
+/*------------------------------------------------------------------------------
+ * Copyright (c) 2018 Arm Limited (or its affiliates). All
+ * rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   1.Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   2.Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   3.Neither the name of Arm nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *------------------------------------------------------------------------------
+ * Name:    GUI_VNC.c
+ * Purpose: 
+ *----------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+#include "main.h"
+#include "Board_LED.h"                  // ::Board Support:LED
+#include "rl_net.h"                     // Keil.MDK-Pro::Network:CORE
+
+extern osThreadId_t TID_app_main;;
+extern void Init_GUIThread(void);
+extern void MQTTEcho_Test (void);
+
+// Main stack size must be multiple of 8 Bytes
+#define APP_MAIN_STK_SZ (4096)
+uint64_t app_main_stk[APP_MAIN_STK_SZ / 8];
+const osThreadAttr_t app_main_attr = {
+  .stack_mem  = &app_main_stk[0],
+  .stack_size = sizeof(app_main_stk)
+};
+
+/* IP address change notification */
+void netDHCP_Notify (uint32_t if_num, uint8_t option, const uint8_t *val, uint32_t len) {
+
+  (void)if_num;
+  (void)val;
+  (void)len;
+
+  if (option == NET_DHCP_OPTION_IP_ADDRESS) {
+    /* IP address change, trigger LCD update */
+    osThreadFlagsSet (TID_app_main, 0x01);
+  }
+}
+
+/*----------------------------------------------------------------------------
+ * Application main thread
+ *---------------------------------------------------------------------------*/
+__NO_RETURN void app_main (void *argument) {
+
+  (void)argument;
+
+  LED_Initialize();
+  netInitialize();
+
+  Init_GUIThread();
+	
+	/*while(1)*/ {
+		char    ip_ascii[40];
+		static uint8_t ip_addr[NET_ADDR_IP4_LEN];
+    /* Wait for signal from DHCP */
+    osThreadFlagsWait (0x01U, osFlagsWaitAny, osWaitForever);
+
+    /* Retrieve and print IPv4 address */
+    netIF_GetOption (NET_IF_CLASS_ETH,
+                     netIF_OptionIP4_Address, ip_addr, sizeof(ip_addr));
+
+    netIP_ntoa (NET_ADDR_IP4, ip_addr, ip_ascii, sizeof(ip_ascii));
+
+    printf ("IP4:%-16s\n",ip_ascii);
+		
+		MQTTEcho_Test();
+	}
+	
+
+  osThreadExit();
+}
---
+++ RTE/_Debug/RTE_Components.h
@@ -0,0 +1,112 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GUI_VNC' 
+ * Target:  'Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.4 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* ARM::Security:mbed TLS:3.1.0 */
+#define RTE_Security_mbedTLS            /* Security mbed TLS */
+/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.5.1 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/* Keil.ARM Compiler::Compiler:I/O:STDIN:User:1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+/* Keil.MDK-Pro::Network:CORE:IPv4/IPv6 Debug:7.18.0 */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_IPv4                /* Network IPv4 Stack */
+          #define RTE_Network_IPv6                /* Network IPv6 Stack */
+          #define RTE_Network_Debug               /* Network Debug Version */
+/* Keil.MDK-Pro::Network:Interface:ETH:7.18.0 */
+#define RTE_Network_Interface_ETH_0     /* Network Interface ETH 0 */
+
+/* Keil.MDK-Pro::Network:Service:DNS Client:7.18.0 */
+#define RTE_Network_DNS_Client          /* Network DNS Client */
+/* Keil.MDK-Pro::Network:Socket:BSD:7.18.0 */
+#define RTE_Network_Socket_BSD          /* Network Socket BSD */
+/* Keil.MDK-Pro::Network:Socket:TCP:7.18.0 */
+#define RTE_Network_Socket_TCP          /* Network Socket TCP */
+/* Keil.MDK-Pro::Network:Socket:UDP:7.18.0 */
+#define RTE_Network_Socket_UDP          /* Network Socket UDP */
+/* Keil::CMSIS Driver:Ethernet MAC:1.12.0 */
+#define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
+/* Keil::CMSIS Driver:Ethernet PHY:LAN8742A:1.3.0 */
+#define RTE_Drivers_PHY_LAN8742A        /* Driver PHY LAN8742A */
+/* Keil::CMSIS Driver:USART:1.21.0 */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver USART4 */
+        #define RTE_Drivers_USART5              /* Driver USART5 */
+        #define RTE_Drivers_USART6              /* Driver USART6 */
+        #define RTE_Drivers_USART7              /* Driver USART7 */
+        #define RTE_Drivers_USART8              /* Driver USART8 */
+/* Keil::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::Device:STM32Cube HAL:Common:1.3.0 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex:1.3.0 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:DMA2D:1.3.0 */
+#define RTE_DEVICE_HAL_DMA2D
+/* Keil::Device:STM32Cube HAL:DMA:1.3.0 */
+#define RTE_DEVICE_HAL_DMA
+/* Keil::Device:STM32Cube HAL:ETH:1.3.0 */
+#define RTE_DEVICE_HAL_ETH
+/* Keil::Device:STM32Cube HAL:GPIO:1.3.0 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:I2C:1.3.0 */
+#define RTE_DEVICE_HAL_I2C
+/* Keil::Device:STM32Cube HAL:LTDC:1.3.0 */
+#define RTE_DEVICE_HAL_LTDC
+/* Keil::Device:STM32Cube HAL:PWR:1.3.0 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:RCC:1.3.0 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:STM32Cube HAL:SDRAM:1.3.0 */
+#define RTE_DEVICE_HAL_SDRAM
+/* Keil::Device:STM32Cube HAL:UART:1.3.0 */
+#define RTE_DEVICE_HAL_UART
+/* Keil::Device:STM32Cube LL:Common:1.3.0 */
+#define RTE_DEVICE_LL_COMMON
+/* Keil::Device:STM32Cube LL:PWR:1.3.0 */
+#define RTE_DEVICE_LL_PWR
+/* Keil::Device:STM32Cube LL:RCC:1.3.0 */
+#define RTE_DEVICE_LL_RCC
+/* Keil::Device:STM32Cube LL:UTILS:1.3.0 */
+#define RTE_DEVICE_LL_UTILS
+/* Keil::Device:Startup:1.2.5 */
+#define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
+/* MDK-Packs::IoT Client:MQTTClient-C:1.1.2 */
+#define RTE_IoT_Client_MQTTClient_C     /* MQTTClient-C IoT Client */
+/* MDK-Packs::IoT Utility:MQTTPacket:1.1.0 */
+#define RTE_IoT_MQTTPacket              /* IoT MQTTPacket */
+/* MDK-Packs::IoT Utility:Socket:MDK Network:1.2.1 */
+#define RTE_IoT_Socket                  /* IoT Socket */
+        #define RTE_IoT_Socket_MDK_Network      /* IoT Socket: MDK::Network */
+/* Segger.MDK-Pro::Graphics:CORE:6.32.3 */
+#define RTE_Graphics_Core               /* Graphics Core */
+/* Segger.MDK-Pro::Graphics:Input Device:Touchscreen:6.32.3 */
+#define RTE_Graphics_Touchscreen        /* Graphics Input Device Touchscreen */
+
+
+#endif /* RTE_COMPONENTS_H */
