10aa4d2ae3a039491909af26ca9faf34b9e4595a
Hans Naert <hans.naert@vives.be>
2024-05-05 17:53:10 +0200

    Update to CMSIS 6.0

--- RTE/_Debug/RTE_Components.h
+++ RTE/_Debug/RTE_Components.h
@@ -16,42 +16,26 @@
  */
 #define CMSIS_device_header "stm32f7xx.h"
 
-/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.4 */
+/* ARM::CMSIS-Compiler:STDIN:Custom@1.0.0 */
+#define RTE_CMSIS_Compiler_STDIN                 /* CMSIS-Compiler STDIN */
+        #define RTE_CMSIS_Compiler_STDIN_Custom          /* CMSIS-Compiler STDIN: Custom */
+/* ARM::CMSIS-Compiler:STDOUT:Custom@1.0.0 */
+#define RTE_CMSIS_Compiler_STDOUT                /* CMSIS-Compiler STDOUT */
+        #define RTE_CMSIS_Compiler_STDOUT_Custom         /* CMSIS-Compiler STDOUT: Custom */
+/* ARM::CMSIS-View:Event Recorder&DAP@1.5.3 */
+#define RTE_CMSIS_View_EventRecorder
+        #define RTE_CMSIS_View_EventRecorder_DAP
+/* ARM::CMSIS:RTOS2:Keil RTX5&Source@5.8.0 */
 #define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
         #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
         #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
-/* ARM::Security:mbed TLS:3.1.0 */
+/* ARM::Security:mbed TLS@3.1.0 */
 #define RTE_Security_mbedTLS            /* Security mbed TLS */
-/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.5.1 */
-#define RTE_Compiler_EventRecorder
-          #define RTE_Compiler_EventRecorder_DAP
-/* Keil.ARM Compiler::Compiler:I/O:STDIN:User:1.2.0 */
-#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
-          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
-/* Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
-#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
-          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
-/* Keil.MDK-Pro::Network:CORE:IPv4/IPv6 Debug:7.18.0 */
-#define RTE_Network_Core                /* Network Core */
-          #define RTE_Network_IPv4                /* Network IPv4 Stack */
-          #define RTE_Network_IPv6                /* Network IPv6 Stack */
-          #define RTE_Network_Debug               /* Network Debug Version */
-/* Keil.MDK-Pro::Network:Interface:ETH:7.18.0 */
-#define RTE_Network_Interface_ETH_0     /* Network Interface ETH 0 */
-
-/* Keil.MDK-Pro::Network:Service:DNS Client:7.18.0 */
-#define RTE_Network_DNS_Client          /* Network DNS Client */
-/* Keil.MDK-Pro::Network:Socket:BSD:7.18.0 */
-#define RTE_Network_Socket_BSD          /* Network Socket BSD */
-/* Keil.MDK-Pro::Network:Socket:TCP:7.18.0 */
-#define RTE_Network_Socket_TCP          /* Network Socket TCP */
-/* Keil.MDK-Pro::Network:Socket:UDP:7.18.0 */
-#define RTE_Network_Socket_UDP          /* Network Socket UDP */
-/* Keil::CMSIS Driver:Ethernet MAC:1.12.0 */
+/* Keil::CMSIS Driver:Ethernet MAC@1.12.0 */
 #define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
-/* Keil::CMSIS Driver:Ethernet PHY:LAN8742A:1.3.0 */
+/* Keil::CMSIS Driver:Ethernet PHY:LAN8742A@1.3.0 */
 #define RTE_Drivers_PHY_LAN8742A        /* Driver PHY LAN8742A */
-/* Keil::CMSIS Driver:USART:1.21.0 */
+/* Keil::CMSIS Driver:USART@1.21.0 */
 #define RTE_Drivers_USART1              /* Driver USART1 */
         #define RTE_Drivers_USART2              /* Driver USART2 */
         #define RTE_Drivers_USART3              /* Driver USART3 */
@@ -60,52 +44,68 @@
         #define RTE_Drivers_USART6              /* Driver USART6 */
         #define RTE_Drivers_USART7              /* Driver USART7 */
         #define RTE_Drivers_USART8              /* Driver USART8 */
-/* Keil::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+/* Keil::Device:STM32Cube Framework:STM32CubeMX@1.1.0 */
 #define RTE_DEVICE_FRAMEWORK_CUBE_MX
-/* Keil::Device:STM32Cube HAL:Common:1.3.0 */
+/* Keil::Device:STM32Cube HAL:Common@1.3.0 */
 #define RTE_DEVICE_HAL_COMMON
-/* Keil::Device:STM32Cube HAL:Cortex:1.3.0 */
+/* Keil::Device:STM32Cube HAL:Cortex@1.3.0 */
 #define RTE_DEVICE_HAL_CORTEX
-/* Keil::Device:STM32Cube HAL:DMA2D:1.3.0 */
+/* Keil::Device:STM32Cube HAL:DMA2D@1.3.0 */
 #define RTE_DEVICE_HAL_DMA2D
-/* Keil::Device:STM32Cube HAL:DMA:1.3.0 */
+/* Keil::Device:STM32Cube HAL:DMA@1.3.0 */
 #define RTE_DEVICE_HAL_DMA
-/* Keil::Device:STM32Cube HAL:ETH:1.3.0 */
+/* Keil::Device:STM32Cube HAL:ETH@1.3.0 */
 #define RTE_DEVICE_HAL_ETH
-/* Keil::Device:STM32Cube HAL:GPIO:1.3.0 */
+/* Keil::Device:STM32Cube HAL:GPIO@1.3.0 */
 #define RTE_DEVICE_HAL_GPIO
-/* Keil::Device:STM32Cube HAL:I2C:1.3.0 */
+/* Keil::Device:STM32Cube HAL:I2C@1.3.0 */
 #define RTE_DEVICE_HAL_I2C
-/* Keil::Device:STM32Cube HAL:LTDC:1.3.0 */
+/* Keil::Device:STM32Cube HAL:LTDC@1.3.0 */
 #define RTE_DEVICE_HAL_LTDC
-/* Keil::Device:STM32Cube HAL:PWR:1.3.0 */
+/* Keil::Device:STM32Cube HAL:PWR@1.3.0 */
 #define RTE_DEVICE_HAL_PWR
-/* Keil::Device:STM32Cube HAL:RCC:1.3.0 */
+/* Keil::Device:STM32Cube HAL:RCC@1.3.0 */
 #define RTE_DEVICE_HAL_RCC
-/* Keil::Device:STM32Cube HAL:SDRAM:1.3.0 */
+/* Keil::Device:STM32Cube HAL:SDRAM@1.3.0 */
 #define RTE_DEVICE_HAL_SDRAM
-/* Keil::Device:STM32Cube HAL:UART:1.3.0 */
+/* Keil::Device:STM32Cube HAL:UART@1.3.0 */
 #define RTE_DEVICE_HAL_UART
-/* Keil::Device:STM32Cube LL:Common:1.3.0 */
+/* Keil::Device:STM32Cube LL:Common@1.3.0 */
 #define RTE_DEVICE_LL_COMMON
-/* Keil::Device:STM32Cube LL:PWR:1.3.0 */
+/* Keil::Device:STM32Cube LL:PWR@1.3.0 */
 #define RTE_DEVICE_LL_PWR
-/* Keil::Device:STM32Cube LL:RCC:1.3.0 */
+/* Keil::Device:STM32Cube LL:RCC@1.3.0 */
 #define RTE_DEVICE_LL_RCC
-/* Keil::Device:STM32Cube LL:UTILS:1.3.0 */
+/* Keil::Device:STM32Cube LL:UTILS@1.3.0 */
 #define RTE_DEVICE_LL_UTILS
-/* Keil::Device:Startup:1.2.5 */
+/* Keil::Device:Startup@1.2.5 */
 #define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
-/* MDK-Packs::IoT Client:MQTTClient-C:1.1.2 */
+/* Keil::Network&MDK-Pro:CORE&IPv4/IPv6 Debug@7.19.0 */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_IPv4                /* Network IPv4 Stack */
+          #define RTE_Network_IPv6                /* Network IPv6 Stack */
+          #define RTE_Network_Debug               /* Network Debug Version */
+/* Keil::Network&MDK-Pro:Interface:ETH@7.19.0 */
+#define RTE_Network_Interface_ETH_0     /* Network Interface ETH 0 */
+
+/* Keil::Network&MDK-Pro:Service:DNS Client@7.19.0 */
+#define RTE_Network_DNS_Client          /* Network DNS Client */
+/* Keil::Network&MDK-Pro:Socket:BSD@7.19.0 */
+#define RTE_Network_Socket_BSD          /* Network Socket BSD */
+/* Keil::Network&MDK-Pro:Socket:TCP@7.19.0 */
+#define RTE_Network_Socket_TCP          /* Network Socket TCP */
+/* Keil::Network&MDK-Pro:Socket:UDP@7.19.0 */
+#define RTE_Network_Socket_UDP          /* Network Socket UDP */
+/* MDK-Packs::IoT Client:MQTTClient-C@1.1.2 */
 #define RTE_IoT_Client_MQTTClient_C     /* MQTTClient-C IoT Client */
-/* MDK-Packs::IoT Utility:MQTTPacket:1.1.0 */
+/* MDK-Packs::IoT Utility:MQTTPacket@1.1.0 */
 #define RTE_IoT_MQTTPacket              /* IoT MQTTPacket */
-/* MDK-Packs::IoT Utility:Socket:MDK Network:1.2.1 */
+/* MDK-Packs::IoT Utility:Socket:MDK Network@1.2.1 */
 #define RTE_IoT_Socket                  /* IoT Socket */
         #define RTE_IoT_Socket_MDK_Network      /* IoT Socket: MDK::Network */
-/* Segger.MDK-Pro::Graphics:CORE:6.32.3 */
+/* Segger::Graphics&MDK-Pro:CORE@6.32.3 */
 #define RTE_Graphics_Core               /* Graphics Core */
-/* Segger.MDK-Pro::Graphics:Input Device:Touchscreen:6.32.3 */
+/* Segger::Graphics&MDK-Pro:Input Device:Touchscreen@6.32.3 */
 #define RTE_Graphics_Touchscreen        /* Graphics Input Device Touchscreen */
 
 
